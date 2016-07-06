09546cafb087a3a887d642b3338808fddc48aa0a
Alexey Podolian <alexeypodolian@gmail.com>
2016-07-06 11:55:32 +0300

    Add sendNativeEvent at RctManager

--- android/app/src/main/java/com/reactnativenavigation/core/RctManager.java
+++ android/app/src/main/java/com/reactnativenavigation/core/RctManager.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.facebook.react.LifecycleState;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.WritableMap;
@@ -16,73 +17,73 @@ import static com.facebook.react.modules.core.DeviceEventManagerModule.RCTDevice
 public class RctManager {
     private static final String KEY_EVENT_ID = "id";
     private static RctManager sInstance;
-
+    
     private ReactInstanceManager mReactManager;
-
+    
     private RctManager() {
         // Singleton
     }
-
+    
     public static synchronized RctManager getInstance() {
         if (sInstance == null) {
             sInstance = new RctManager();
         }
         return sInstance;
     }
-
+    
     public static void destroy() {
         if (sInstance == null) return;
         
         sInstance.mReactManager = null;
         sInstance = null;
     }
-
+    
     public ReactInstanceManager getReactInstanceManager() {
         return mReactManager;
     }
-
+    
     public boolean isInitialized() {
         return mReactManager != null;
     }
-
+    
     public void init(BaseReactActivity context) {
         createReactInstanceManager(context);
     }
-
+    
     /**
      * Creates a React Instance Manager associated with this component name
      */
     public ReactInstanceManager createReactInstanceManager(BaseReactActivity reactActivity) {
         ReactInstanceManager.Builder builder = ReactInstanceManager.builder()
-                .setApplication((Application) reactActivity.getApplicationContext())
-                .setJSMainModuleName(reactActivity.getJSMainModuleName())
-                .setUseDeveloperSupport(reactActivity.getUseDeveloperSupport())
-                .setInitialLifecycleState(LifecycleState.BEFORE_RESUME);
-
+        .setApplication((Application) reactActivity.getApplicationContext())
+        .setJSMainModuleName(reactActivity.getJSMainModuleName())
+        .setUseDeveloperSupport(reactActivity.getUseDeveloperSupport())
+        .setInitialLifecycleState(LifecycleState.BEFORE_RESUME);
+        
         for (ReactPackage reactPackage : reactActivity.getPackages()) {
             builder.addPackage(reactPackage);
         }
-
+        
         String jsBundleFile = reactActivity.getJSBundleFile();
-
+        
         if (jsBundleFile != null) {
             builder.setJSBundleFile(jsBundleFile);
         } else {
             builder.setBundleAssetName(reactActivity.getBundleAssetName());
         }
-
+        
         mReactManager = builder.build();
         return mReactManager;
     }
-
+    
     public <T extends ReactContextBaseJavaModule> T getNativeModule(Class<T> nativeModuleClass) {
         if (mReactManager == null || mReactManager.getCurrentReactContext() == null) {
             return null;
         }
-
+        
         return mReactManager.getCurrentReactContext().getNativeModule(nativeModuleClass);
     }
-
+    
     /**
      * Sends an event to JavaScript using <a href="https://facebook.github.io/react-native/docs/native-modules-android.html#sending-events-to-javascript">RCTDeviceEventEmitter</a>
      *
@@ -95,22 +96,30 @@ public class RctManager {
         if (eventEmitter == null) {
             return;
         }
-
+        
         params.putString(KEY_EVENT_ID, eventName);
         params.putString(Screen.KEY_NAVIGATOR_EVENT_ID, screen.navigatorEventId);
         eventEmitter.emit(screen.navigatorEventId, params);
     }
-
+    
+    public void sendNativeEvent(String eventName, WritableMap params) {
+        RCTDeviceEventEmitter eventEmitter = getEventEmitter();
+        if (eventEmitter == null) {
+            return;
+        }
+        eventEmitter.emit(eventName, params);
+    }
+    
     private RCTDeviceEventEmitter getEventEmitter() {
         if (mReactManager == null) {
             return null;
         }
-
+        
         ReactContext currentReactContext = mReactManager.getCurrentReactContext();
         if (currentReactContext == null) {
             return null;
         }
-
+        
         return currentReactContext.getJSModule(RCTDeviceEventEmitter.class);
     }
 }
